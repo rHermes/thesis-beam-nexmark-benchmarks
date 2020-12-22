23ff718ff8e38caf0b5a0956e18efbcd1d630baa
rHermes <teodor_spaeren@riseup.net>
2020-12-22 16:30:54 +0100

    Small changes

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/rhermes/beam-nexmark-benchmarks
 
 go 1.15
+
+require (
+	github.com/alecthomas/repr v0.0.0-20201120212035-bb82daffcca2
+	github.com/flink-go/api v0.0.1
+)
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os/exec"
 	"strings"
+	"time"
+
+	"github.com/alecthomas/repr"
+	"github.com/flink-go/api"
 )
 
 const (
@@ -14,26 +19,106 @@ const (
 )
 
 type Benchmark struct {
+	FlinkMaster        string
+	JavascriptFilename string
+	Query              string
+}
+
+func (b *Benchmark) Run(gradlePath, beamPath string) ([]byte, error) {
+	return nil, errors.New("Not implemented yet")
+}
+
+func simpleMetrics(c *api.Client, jobID string) error {
+	res, err := c.Job(jobID)
+	if err != nil {
+		return err
+	}
+	repr.Println(res)
+
+	// met, err := c.JobMetrics(api.JobMetricsOpts{
+	// 	Jobs: []string{jobID},
+	// })
+	// if err != nil {
+	// 	return err
+	// }
+
+	// fmt.Println("==== WTF ====")
+	// repr.Println(met)
+
+	return nil
+}
+
+func cc() error {
+	c, err := api.New("127.0.0.1:8081")
+	if err != nil {
+		return err
+	}
+
+	config, err := c.Config()
+	if err != nil {
+		return err
+	}
+
+	repr.Println(config)
+
+	for {
+		jobs, err := c.Jobs()
+		if err != nil {
+			return err
+		}
+
+		log.Printf("Jobs:\n")
+		for _, job := range jobs.Jobs {
+			if job.Status == "FINISHED" || job.Status == "FAILED" {
+				continue
+			}
+			repr.Print(job)
+
+			if err := simpleMetrics(c, job.ID); err != nil {
+				return err
+			}
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	return nil
 }
 
 func main() {
+	go func() {
+		if err := cc(); err != nil {
+			log.Fatalf("THis happened: %s\n", err.Error())
+		}
+	}()
 	nexmarkArgs := []string{
 		"--runner=FlinkRunner",
 		"--streaming=true",
-		"--streamTimeout=60",
-		"--flinkMaster=[local]",
+		// "--streamTimeout=600",
+		// "--flinkMaster=[local]",
+		"--flinkMaster=localhost",
 		"--manageResources=false",
 		"--monitorJobs=true",
 		"--javascriptFilename=" + OutputPath + "/wow.data.js",
-		"--query=1",
+		"--query=PASSTHROUGH",
 		"--fasterCopy=true",
+		// "--numEvents=0",
+		// "--isRateLimited=true",
+		// "--firstEventRate=200",
+		// "--numEvents=10000000000",
+		// "--numEvents=10000",
+		"--numEvents=100000000",
+		// "--numEvents=10000000000",
+		// "--rateUnit=PER_MINUTE",
 	}
 
 	args := []string{
 		"-p", BeamPath,
 		"-Pnexmark.runner=:runners:flink:1.10",
 		"-Pnexmark.args=" + strings.Join(nexmarkArgs, "\n"),
-		":sdks:java:testing:nexmark:run"}
+		":sdks:java:testing:nexmark:run",
+	}
+	args = args
+
 	c := exec.Command(GradlePath, args...)
 	o, err := c.CombinedOutput()
 	if err != nil {
